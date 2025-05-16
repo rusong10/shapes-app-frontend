7a6c955f6575764c6675e2094da82615d04ee3f9
rusong <phuahrusong@live.com>
2025-05-16 08:17:53 +0800

    Updated API Links

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,10 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default Object.freeze({
-    LOGIN: `${API_BASE_URL}/api/auth/login/`,
-    LOGOUT: `${API_BASE_URL}/api/auth/logout/`,
-    TOKEN_REFRESH: `${API_BASE_URL}/api/auth/token/refresh/`,
-    TOKEN_VERIFY: `${API_BASE_URL}/api/auth/token/verify/`,
+    LOGIN: `${API_BASE_URL}/api/accounts/login/`,
+    LOGOUT: `${API_BASE_URL}/api/accounts/logout/`,
+    TOKEN_REFRESH: `${API_BASE_URL}/api/accounts/token/refresh/`,
+    TOKEN_VERIFY: `${API_BASE_URL}/api/accounts/token/verify/`,
     SHAPES: `${API_BASE_URL}/api/shapes/`,
     SHAPE_BY_ID: (id: number) => `${API_BASE_URL}/api/shapes/${id}/`
 });
